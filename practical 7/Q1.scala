9bd0506cbe91e75a893dd5703809d1011dbccebf
sakiththewmika <sakiththewmika@gmail.com>
2024-08-12 20:55:07 +0530

    practical 7 code edit

--- practical 7/Q1.scala
+++ practical 7/Q1.scala
@@ -4,7 +4,19 @@ object EvenNumberFilter {
     }
 
     def main(args : Array[String]) : Unit = {
-        val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        // val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        var input = List[Int]();
+        var continue = true;
+        while (continue) {
+        print("Enter a number (0 to stop): ")
+        val num = scala.io.StdIn.readInt();
+        if (num == 0) {
+            continue = false;
+        } else {
+            input = input :+ num;
+        }
+        }
+
         val output = filterEvenNumbers(input);
         println("Even numbers : " + output);
     }
